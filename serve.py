0176b9eaee36010c8acde4f8964bd19430a65351
gouravagra <goluagrawal1432@gmail.com>
2024-12-06 14:07:46 +0530

    port changes

--- serve.py
+++ serve.py
@@ -58,4 +58,5 @@ async def translate(input: TranslationInput, request: Request):
 # Main entry point
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
